dd3266a05425124a88b1efb4b8da799aa95227d7
Brandon Tiley <bctiley@gmail.com>
2024-04-16 15:47:42 +1000

    Update Covid_Analysis_Queries_For_Tableau.sql

--- Covid_Analysis_Queries_For_Tableau.sql
+++ Covid_Analysis_Queries_For_Tableau.sql
@@ -9,17 +9,6 @@ WHERE continent is not null
 --GROUP BY date
 ORDER BY 1,2
 
--- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
-
-
---SELECT SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
---FROM [COVID_19 Portfolio Project]..CovidDeaths
-----WHERE location like '%states%'
---WHERE location = 'World'
-----GROUP BY date
---ORDER BY 1,2
-
 -- 2. TotalDeathCount per continent
 
 -- We take these out as they are not inluded in the above queries and want to stay consistent
